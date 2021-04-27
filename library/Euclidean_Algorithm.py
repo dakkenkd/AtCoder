f94913a0646a8f7e09e13458fc8848635c6fafa1
TyamaKodai <64186407+TyamaKodai@users.noreply.github.com>
2021-04-27 11:53:43 +0900

    added code of Euclidean Algorithm

---
+++ library/Euclidean_Algorithm.py
@@ -0,0 +1,22 @@
+# Euclidean Algorithm
+def gcd(a,b):
+  r = a % b # a >=b としてあまりを求める 
+  return gcd(b, r) if r else b # rがゼロでないのならば、b と r のgcdを返し、rがゼロであればbが最大公約数になる
+
+# Euclidean Algorithm (non-recursive)
+def gcd2(a,b):
+  while b:
+    a, b = b, a%b
+  return a
+
+# Extended Euclidean Algorithm
+def extgcd(a,b):
+  if b:
+    d, y, x = extgcd(b, a%b)
+    y -= (a//b)*x
+    return d, x, y
+  return a, 1, 0
+
+# lcm (least common multiple)
+def lcm(a,b):
+  return a//gcd(a,b)*b
