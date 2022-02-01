a37d128ca5734ef9b06d5b4d205d01a195e1dce7
dakkenkd <dakkenkd425@gmail.com>
2022-02-01 20:18:42 +0900

    add dijkstra

---
+++ library/dijkstra.py
@@ -0,0 +1,27 @@
+from heapq import *
+n, m = map(int, input().split())
+g = [[] for _ in range(n)]
+
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    a -= 1
+    b -= 1
+    g[a].append((c, b))
+    g[b].append((c, a))
+
+hq = [(-1, 0)]
+dist = [float("inf")] * n
+path = [-1] * n
+dist[0] = 0
+
+while hq:
+    c, v = heappop(hq)
+    if dist[v] != c: continue
+    for cc, vv in g[v]:
+        if dist[vv] > dist[v] + cc:
+            dist[vv] = dist[v] + cc
+            path[vv] = v
+            heappush(hq, (dist[vv], vv))
+
+print(path)
+print(dist)
