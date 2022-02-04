66ba97b6d9e161c0ac0c5dbbf910fd513e4d40a1
dakkenkd <dakkenkd425@gmail.com>
2022-02-04 11:00:08 +0900

    add code

--- 競プロ典型90問/021.py
+++ 競プロ典型90問/021.py
@@ -1,54 +1,50 @@
 from collections import deque
 import sys
-sys.setrecursionlimit(10**6)
-def root(x):
-    if par[x] < 0:
-        return x
-    else:
-        par[x] = root(par[x])
-        return par[x]
-
-def union(x, y):
-    xx = root(x)
-    yy = root(y)
-    if xx == yy:
-        return
-    else:
-        par[xx] += par[yy]
-        par[yy] = xx
-
-def same(x, y):
-    xx = root(x)
-    yy = root(y)
-    return xx == yy
+sys.setrecursionlimit(10**7)
 
 n, m = map(int, input().split())
 g = [[] for _ in range(n)]
+rg = [[] for _ in range(n)]
 edges = []
-for i in range(n):
+for i in range(m):
     a, b = map(int, input().split())
     g[a-1].append(b-1)
-    edges.append([a,b])
+    rg[b-1].append(a-1)
 
 dist = [-1] * n
-par = [-1] * n
+come = [-1] * n
+backorder = [-1] * n
+components = []
 
-# 道をたどって同じ連結成分に入れる
-def union_nodes(v):
-    if not same(v, dist[v]):
-        union(v, dist[v])
-        union_nodes(dist[v])
+def dfs(v):
+    come[v] = True
+    for to in g[v]:
+        if come[to]: continue
+        dfs(to)
+    backorder.append(v)
+
+def rdfs(v):
+    come[v] = True
+    components[-1].append(v)
+    for to in rg[v]:
+        if come[to]: continue
+        rdfs(to)
 
 for i in range(n):
-    if dist[i] != -1: continue
-    q = deque([i])
-    while q:
-        v = q.pop()
-        for vv in g[v]:
-            if dist[vv] != -1:
-                union_nodes(vv)
-            dist[vv] = v
-            q.append(vv)
+    if come[i]: continue
+    dfs(i)
 
-print(par)
-print(dist)
+backorder.reverse()
+come = [-1] * n
+for v in backorder:
+    if come[v]: continue
+    components.append([])
+    rdfs(v)
+
+ans = 0
+
+for component in components:
+    n = len(component)
+    ans += n * (n-1) // 2
+
+print(ans)
