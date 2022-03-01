f534041a62abe0a6fefa32d8237ca560f710f25e
dakkenkd <dakkenkd425@gmail.com>
2022-03-01 22:01:17 +0900

    update BIT

--- library/BIT.py
+++ library/BIT.py
@@ -1,54 +1,50 @@
-n = 5
-lst = [8,6,4,3,2]
 
-"""
-l 以上 r以下の値を調べたいときは配列をソートしてからBITを作り、
-l, r = bisect_left(lst, ), bisect_right(lst, 10) - 1
-みたいな感じで区間を絞ってください
-"""
-
-from collections import Counter
-class FenwickTree:
-
-    def __init__(self, n, lst):
-        self.arr = lst
-        self.x2i = {d:i+1 for i, d in enumerate(sorted(set(lst)))}
-        self.i2x = {i+1:d for i, d in enumerate(sorted(set(lst)))}
-        self.comp_arr = [self.x2i[d] for d in lst]
-        self.bit = [0] * (len(lst) + 1)
+# Binary Indexed Tree (Fenwick Tree)
+# @kuruton456
+class BIT:
+    def __init__(self, n):
         self.n = n
+        self.n0 = 2**(n-1).bit_length()
+        self.data = [0]*(n+1)
+        self.el = [0]*(n+1)
+    def init(self, A):
+        self.data[1:] = A
+        for i in range(1, self.n):
+            if i + (i & -i) <= self.n:
+                self.data[i + (i & -i)] += self.data[i]
+    def sum(self, i):
+        s = 0
+        while i > 0:
+            s += self.data[i]
+            i -= i & -i
+        return s
+    def add(self, i, x):
+        # assert i > 0
+        self.el[i] += x
+        while i <= self.n:
+            self.data[i] += x
+            i += i & -i
+    def get(self, i, j=None):
+        if j is None:
+            return self.el[i]
+        return self.sum(j) - self.sum(i)
+    def lower_bound(self, x):
+        w = i = 0
+        k = self.n0
+        """ 累積和がx以上になる最小のindexと、その直前までの累積和 """
+        while k:
+            print("k =", k)
+            if i+k <= self.n and w + self.data[i+k] < x:
+                w += self.data[i+k]
+                i += k
+            k >>= 1
+        # assert self.get(0, i) <= x < self.get(0, i+1)
+        return i+1, w
 
-    def bit_add(self, x, w): # x: 加算したい場所, w: 加算したい値
-        x += 1
-        while x <= self.n:
-            self.bit[x] += w
-            x += x & -x
+n = 5
 
-    def bit_sum(self, l, r): # (l, r) の合計
-        r += 1
-        r_sum = 0
-        l_sum = 0
-        while r > 0:
-            r_sum += self.bit[r] # 現在のブロックの値を足していく
-            r -= r & -r # 左上のブロックに移動していく
-        while l > 0:
-            l_sum += self.bit[l]
-            l -= l & -l
-        return r_sum - l_sum
-
-
-
-BIT = FenwickTree(n, lst)
-
-
-# 二分探索
-# NO = 2**(n-1).bit_length()
-# def lower_bound(x):
-#     w = i = 0
-#     k = NO
-#     while k > 0:
-#         if i + k <= n and w + bit[i + k] <= x:
-#             w += bit[i + k]
-#             i += k
-#         k >>= 1
-#     return i + 1
+bit = BIT(n)
+for i in range(5):
+    bit.add(i+1, 1)
+print(bit.data)
+print(bit.lower_bound(2))
