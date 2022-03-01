e2635959bccdfe7f6918a191690914be9a65dc45
dakkenkd <dakkenkd425@gmail.com>
2022-03-01 22:01:36 +0900

    memo

--- memo.py
+++ memo.py
@@ -1,81 +1,76 @@
-from bisect import bisect_left, bisect_right
-
-class FenwickTree:
-
-    def __init__(self, n, lst):
-        self.arr = lst
-        self.x2i = {d:i+1 for i, d in enumerate(sorted(set(lst)))}
-        self.i2x = {i+1:d for i, d in enumerate(sorted(set(lst)))}
-        self.comp_arr = [self.x2i[d] for d in lst]
-        self.bit = [0] * (len(lst) + 10)
+# Binary Indexed Tree (Fenwick Tree)
+class BIT:
+    def __init__(self, n):
         self.n = n
+        self.n0 = 2**(n-1).bit_length()
+        self.data = [0]*(n+1)
+        self.el = [0]*(n+1)
+    def init(self, A):
+        self.data[1:] = A
+        for i in range(1, self.n):
+            if i + (i & -i) <= self.n:
+                self.data[i + (i & -i)] += self.data[i]
+    def sum(self, i):
+        s = 0
+        while i > 0:
+            s += self.data[i]
+            i -= i & -i
+        return s
+    def add(self, i, x):
+        # assert i > 0
+        self.el[i] += x
+        while i <= self.n:
+            self.data[i] += x
+            i += i & -i
+    def get(self, i, j=None):
+        if j is None:
+            return self.el[i]
+        return self.sum(j) - self.sum(i)
+    def lower_bound(self, x):
+        w = i = 0
+        k = self.n0
+        while k:
+            if i+k <= self.n and w + self.data[i+k] < x:
+                w += self.data[i+k]
+                i += k
+            k >>= 1
+        # assert self.get(0, i) <= x < self.get(0, i+1)
+        return i+1
 
-    def bit_add(self, x, w): # x: 加算したい場所, w: 加算したい値
-        x = self.x2i[x]
-        while x <= self.n:
-            self.bit[x] += w
-            x += x & -x
-
-    def bit_sum(self, l, r):
-        l, r = self.x2i[l]-1, self.x2i[r]
-        r_sum = 0
-        l_sum = 0
-        while r > 0:
-            r_sum += self.bit[r] # 現在のブロックの値を足していく
-            r -= r & -r # 左上のブロックに移動していく
-        while l > 0:
-            l_sum += self.bit[l]
-            l -= l & -l
-        return r_sum - l_sum
-
-# bit_sum() に入れるのは index なので, BIT.x2i[x] などで座標圧縮後の index に直してからつっこむ
 
 q = int(input())
 query = []
-X = []
+X = set()
 
 # クエリ先読み
 for _ in range(q):
     L = tuple([*map(int, input().split())])
-    X.append(L[1])
+    X.add(L[1])
     if L[0] != 1:
-        X.append(L[2])
+        X.add(L[2])
     query.append(L)
-X.sort()
-BIT = FenwickTree(len(X), X)
+
+# 座標圧縮
+x2i = {x:i for i, x in enumerate(sorted(X), 1)}
+i2x = {i:x for i, x in enumerate(sorted(X), 1)}
+n = len(X)
+bit = BIT(n)
 
 for d in query:
     if d[0] == 1:
-        BIT.bit_add(d[1], 1)
+        bit.add(x2i[d[1]], 1)
     elif d[0] == 2:
         x, k = d[1], d[2]
-        idx = BIT.x2i[x]
-        ok = 0
-        ng = idx
-        s = BIT.bit_sum(0, idx)
-        if s < k:
+        tmp = bit.sum(x2i[x])
+        if tmp < k:
             print(-1)
-            continue
-        while ng - ok > 1:
-            mid = (ng+ok)//2
-            if BIT.bit_sum(mid, idx) >= k:
-                ok = mid
-            else:
-                ng = mid
-        print(BIT.i2x[ok])
+        else:
+            print(i2x[bit.lower_bound(tmp - k)])
     else:
         x, k = d[1], d[2]
-        idx = BIT.x2i[x]
-        ng = idx-1
-        ok = BIT.n
-        s = BIT.bit_sum(idx, BIT.n)
-        if s < k:
+        total = bit.sum(n)
+        l = bit.sum(max(0, x2i[x]-1))
+        if total - l < k:
             print(-1)
-            continue
-        while ok - ng > 1:
-            mid = (ok+ng) // 2
-            if BIT.bit_sum(idx-1, mid) >= k:
-                ok = mid
-            else:
-                ng = mid
-        print(BIT.i2x[ok])
+        else:
+            print(i2x[bit.lower_bound(l+k)])
