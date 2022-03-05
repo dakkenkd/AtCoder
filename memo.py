3e2410a29820b9fd942f1cec2b7280027f42be07
dakkenkd <dakkenkd425@gmail.com>
2022-03-05 21:10:07 +0900

    addcode

--- memo.py
+++ memo.py
@@ -1,26 +1,7 @@
-n, m  = map(int, input().split())
-A = [list(input()) for _ in range(n)]
-B = [list(input()) for _ in range(m)]
-ans = False
-if n == m:
-    for i in range(n):
-        for j in range(n):
-            flag = True
-            if A[i][j] != B[i][j]:
-                flag = False
-    if flag:
-        ans = True
+a,b,c,x = map(int, input().split())
+if x <= a:
+    print(1)
+elif a+1 <= x <= b:
+    print(1/(b-(a+1)+1))
 else:
-    for i in range(n-m):
-        for j in range(n-m):
-            flag = True
-            for ii in range(i, i+m):
-                for jj in range(j, j+m):
-                    if A[ii][jj] != B[ii-i][jj-j]:
-                        flag = False
-            if flag:
-                ans = True
-if ans:
-    print("Yes")
-else:
-    print("No")
+    print(0)
