f7021929ca38b0724e0d5beaa5925f763ff91364
dakkenkd <dakkenkd425@gmail.com>
2022-03-13 18:30:09 +0900

    clear

--- memo.py
+++
@@ -1,7 +0,0 @@
-a,b,c,x = map(int, input().split())
-if x <= a:
-    print(1)
-elif a+1 <= x <= b:
-    print(1/(b-(a+1)+1))
-else:
-    print(0)
--- memo2.py
+++
@@ -1,3 +0,0 @@
-s = list(input())
-s.sort()
-print("".join(s))
