6a0654e58f9cba4955814536827a8b77bcf9db56
dakkenkd <dakkenkd425@gmail.com>
2022-10-19 22:19:01 +0900

    change uf

--- library/UnionFind/UnionFind.py
+++ library/UnionFind/UnionFind.py
@@ -1,24 +1,22 @@
-n = int(input())
-par = [i for i in range(1+n)]
-
-# find (経路圧縮)
-def find(x):
-    if par[x] == x:
+import sys
+sys.setrecursionlimit(10**6)
+def root(x):
+    if par[x] < 0:
         return x
     else:
-        par[x] = find(par[x])
+        par[x] = root(par[x])
         return par[x]
 
-# お互いの親が一緒であるかを確認して同じグループかどうかを判断する
-def same(x,y):
-    return find(x) == find(y)
+def union(x, y):
+    xx = root(x)
+    yy = root(y)
+    if xx == yy:
+        return
+    else:
+        par[xx] += par[yy]
+        par[yy] = xx
 
-# それぞれの親を確認して異なる場合のみ親を統一する
-def unite(x,y):
-    x = find(x)
-    y = find(y)
-    if x == y:
-        return 0
-    if x < y:
-        x, y = y, x
-    par[x] = y
+def same(x, y):
+    xx = root(x)
+    yy = root(y)
+    return xx == yy
--- 競プロ典型90問/037.py
+++ 競プロ典型90問/037.py
@@ -1,33 +1,82 @@
-from collections import deque
+def segfunc(x, y):
+    return max(x, y)
+"""
+操作 segfunc 単位元
+最小値 min(x,y) math.inf
+最大値 max(x,y) -math.inf
+区間和 x+y 0
+区間積 x*y 1
+最大公約数 math.gcd(x,y) 0
+"""
+class SegTree:
+    def __init__(self, init_val, segfunc, ide_ele):
+        n = len(init_val)
+        self.segfunc = segfunc
+        self.ide_ele = ide_ele
+        self.num = 1<<(n-1).bit_length()
+        self.tree = [ide_ele] * (2 * self.num)
+
+        # セグ木の初期化
+        for i in range(n):
+            self.tree[self.num+i] = init_val[i]
+
+        # 上まで値を計算しておく
+        for i in range(self.num-1, 0, -1):
+            self.tree[i] = self.segfunc(self.tree[2*i], self.tree[2*i+1])
+
+    def add(self, k, x):
+        k += self.num
+        self.tree[k] += x
+        while k > 1:
+            self.tree[k>>1] = self.segfunc(self.tree[k], self.tree[k^1])
+            k >>= 1
+
+    def update(self, k, x):
+        k += self.num
+        self.tree[k] = x
+        while k > 1:
+            self.tree[k>>1] = self.segfunc(self.tree[k], self.tree[k^1])
+            k >>= 1
+
+    def query(self, l, r):
+        # [l, r)
+        res = self.ide_ele
+        l += self.num
+        r += self.num
+        while l < r:
+            if l & 1:
+                res = self.segfunc(res, self.tree[l])
+                l += 1
+            if r & 1:
+                res = self.segfunc(res, self.tree[r-1])
+            l >>= 1
+            r >>= 1
+        return res
+
+
 w, n = map(int, input().split())
+L = [-1] * (w+1)
+L[0] = 0
+# index -> 香辛料の合計
+# 要素 -> 価値の最大値
+memo = SegTree(L, segfunc, 0)
 
-dp = [-1] * (w+1)
-dp[0] = 0
+dp = [0] * (w+1)
 
-for _ in range(n):
+for i in range(n):
     l, r, v = map(int, input().split())
-    # iv ... dpの区間[i-r, i-l]の最大値を得るためのスライド最大値
-    # ivとは(index, value)を意味してる
-    iv = deque()
-
-    # スライド最大値の初期化
-    for i in range(w-l, w-r, -1):
-        while iv and iv[-1][-1] <= dp[i]:
-            iv.pop()
-        iv.append((i, dp[i]))
-
-    # DP本体
-    for i in range(w, l-1, -1):
-        # スライド最大値に対する処理
-        if iv and iv[0][0] == i-l+1:
-            iv.popleft()
-        if i-r >= 0:
-            while iv and iv[-1][-1] <= dp[i-r]:
-                iv.pop()
-            iv.append((i-r, dp[i-r]))
-        if iv[0][1] == -1: continue
-
-        # DP更新
-        dp[i] = max(dp[i], iv[0][1]+v)
-
-print(dp[1])
+    new = dp[:]
+    for ww in range(w+1):
+        ll = max(0, ww - r)
+        rr = max(0, ww - l)
+        x = memo.query(ll, rr+1)
+        if x == -1: continue
+        new[ww] = max(new[ww], max(dp[ww], v + x))
+        
+    for ww in range(w+1):
+        memo.update(ww,new[ww])
+    dp = new
+if new[w] == 0:
+    print(-1)
+else:
+    print(new[w])
